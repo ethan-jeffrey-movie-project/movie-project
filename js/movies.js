9b50986063d9298a3d6ac56f1f5c1aeb77eccfce
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-07 09:30:37 -0600

    added files

--- js/movies.js
+++ js/movies.js
@@ -1,133 +1,213 @@
 
+	"use strict";
+	$(document).ready(function () {
+	//first call refresh movies
+	refreshMovies();
 
+	function refreshMovies() {
+	document.getElementById('movieDisplay').innerHTML = `<div class="text-center"><h1 style="color:var(--pink2)">LOADING...</h1></div>`;
+	fetch('https://dandy-plucky-cover.glitch.me/movies').then(response => {
+	//html is going to display on id with moviedisplay
+	$('#movieDisplay').html('');
+	response.json().then(movies => {
+	console.log(movies);
+	document.getElementById('movieDisplay').innerHTML = '';
+	movies.forEach(movieObj => {
+	//appending the function createImageFromGlitch(movieObj) result to movie display
+	$('#movieDisplay').append(`${createImageFromGlitch(movieObj)}`);
+	//connect delete
+	console.log("Iterating over id: " + movieObj.id);
 
-//glitch url
-// const G_Url = 'https://vivid-flawless-almandine.glitch.me/movies';
+	//when the id of movie obj is clicked do the following
+	$(`#${movieObj.id}`).click(function (e) {
+	e.preventDefault();
+	console.log(`${movieObj.id}`);
+	let deleteID = movieObj.id;
 
-const G_Url = 'https://gravel-rainbow-hammer.glitch.me/movies';
+	console.log(`deleting id  ${movieObj.id}`)
+	fetch(`https://dandy-plucky-cover.glitch.me/movies/${deleteID}`, {
+	method: 'DELETE',
+}).then(result => {
 
-
-
-// const options = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(reviewObj),
-// };
-
-fetch(G_Url)
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-
-
-
-// const addedMovie = {
-//
-//     name: 'Halloween',
-//     rating: 4,
-//     comments: "best slasher movie"
-// };
-// const url = 'https://codeup-json-server.glitch.me/movies/';//user object selection they want to delete
-//
-// const options = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(reviewObj),
-// };
-// fetch(url)
-//     // .then( response => console.log(response) ) /* review was created successfully */
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch( error => console.error(error) ); /* handle errors */
-
-// need userinput whenever they want to add a movies
-// user needs a ratting and a title
-//only display objects that have a title and a ratting
-//need an event listener when the user submits the form
-//needs to be a asyncronus operation, and not reload the entire page
-
-
-
-
-//when user deletes their object by selecting id
-// const url = ' https://codeup-json-server.glitch.me/movies';//user object selection they want to delete
-
-// fetch(url,{
-//     method: 'DELETE'
-// }).then(res => res.json()).then(data => console.log(data))
-//
-
-
-// fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch(error => console.error(error))
-
-
-fetch(G_Url,{
-    method: 'DELETE'
-}).then(res => res.json()).then(data => console.log(data))
-
-
-
-fetch(G_Url)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch(error => console.error(error))
-
-
-
-function movieAdd() {
-    $("#movies").append("<tr>" +
-        "<td>My First Video</td>" +
-        "<td>6/11/2015</td>" +
-        "<td>www.pluralsight.com</td>" +
-        "</tr>");
+	console.log(`completed deleting ${deleteID}`);
+	console.log(result);
+	refreshMovies();
+});
+});
+});
+});
+});
 }
 
+	//CARD LAYOUT
+	function createImage(movies){
+	let html =`
+				<div class="col-3">
+					<div class="card bg-dark text-white mb-2 cardMinHeight text-wrap">
+						<img src="${movies.Poster}" class="card-img imgOpacity cardHeight" alt="Broken Image">
+							<div class="card-img-overlay">
+								<h3 class="card-title">${movies.Title}</h3>
+								<p class="card-text"><strong>Rated</strong>: ${movies.Rated}</p>
+							</div>
+					</div>
+				</div>`;
+	return html;
+}
+
+	function createImageFromGlitch(movies){
+	let html =`
+				<div class="col-3 hovEff" >
+					<div class="card bg-dark text-white mb-2 cardMinHeight text-wrap overflow-auto " style="border-radius: 13%">
+						<img src="${movies.poster}" class="card-img imgOpacity cardHeight " alt="Broken Image">
+							<div class="card-img-overlay ">
+								<h3 class="card-title"> ${movies.title}</h3>
+								<p class="card-text"><strong>Rating</strong>: ${movies.rating}</p>
+								<p class="card-text"><strong>Movie ID</strong>: ${movies.id}</p>
+								<button class="btn btn-danger  colorBtn" id="${movies.id}">DELETE</button>
+							</div>
+					</div>
+				</div>`;
+	return html;
+}
+
+	//ADD FORM BTN START
+	$('#addMovieBtn').click(function (e) {
+	e.preventDefault();
+	$('#addMovieBtn').attr('disabled', true);
+	let movieObj = {};
+	movieObj.title = $('#inputTitle').val();
+	if (movieObj.title === '' || movieObj.title === undefined) {
+	movieObj.title = 'N/A';
+}
+	movieObj.rating = $('#inputRating').val();
+	if (movieObj.rating === '' || movieObj.rating === undefined) {
+	movieObj.rating = 'N/A';
+}
+	movieObj.poster = $('#inputPosterLink').val();
+	if (movieObj.poster === '' || movieObj.poster === undefined) {
+	movieObj.poster = 'img/....jpeg';
+}
+	console.log("showing final object before add");
+	console.log(movieObj);
+	$('#movieDisplay').prepend(createImage(movieObj));
+	const url = 'https://dandy-plucky-cover.glitch.me/movies';
+	const options = {
+	method: 'POST',
+	headers: {
+	'Content-Type': 'application/json',
+},
+	body: JSON.stringify(movieObj),
+};
+	fetch(url, options)
+	.then(response => {
+	$('#addMovieBtn').attr('disabled', false);
+	return response.json();
+}).then(movies => {
+	console.log("printing the result after post");
+	console.log(movies);
+	refreshMovies();
+})
+	.catch(error => console.error(error));
+});
+	//ADD FORM BTN END
+
+	$('#editChooseBtn').click(function (e) {
+	e.preventDefault();
+	let editID = document.getElementById('editIDInput').value;
+	console.log('ID to be edited:' + editID);
+	fetch(`https://dandy-plucky-cover.glitch.me/movies/${editID}`, {
+	method: 'GET',
+}).then(result => result.json().then(final => {
+	console.log("showing final object");
+	console.log(final);
+	//TARGETS THE CORRESPONDING ID'S
+	document.getElementById('inputEditTitle').value = final.title;
+	document.getElementById('inputEditRating').value = final.rating;
+	document.getElementById('inputEditPosterLink').value = final.poster;
+}));
+
+});
+
+	$('#editMovieSubBtn').click(function (e) {
+	e.preventDefault();
+	console.log("start edit submit");
+	let editID = document.getElementById('editIDInput').value;
+	let movieObj = {};
+	movieObj.title = document.getElementById('inputEditTitle').value
+	movieObj.rating = document.getElementById('inputEditRating').value;
+	movieObj.poster = document.getElementById('inputEditPosterLink').value;
+	console.log(movieObj);
+	const url = `https://dandy-plucky-cover.glitch.me/movies/${editID}`;
+	const options = {
+	method: 'PATCH',
+	headers: {
+	'Content-Type': 'application/json',
+},
+	body: JSON.stringify(movieObj),
+};
+	fetch(url, options)
+	.then(response => response.json().then(movies => {
+	refreshMovies();
+}))  /* review was created successfully */
+	.catch(error => console.error(error)); /* handle errors */
+});
+
+	//work in tandem with add movie
+
+	$('#deleteSubBtn').click(function (e) {
+	e.preventDefault();
+	console.log(document.getElementById('deleteIDinput').value);
+	let deleteID = (document.getElementById('deleteIDinput').value);
+	document.getElementById('deleteIDinput').value = '';
+	fetch(`https://dandy-plucky-cover.glitch.me/movies/${deleteID}`, {
+	method: 'DELETE',
+}).then(result => {
+	console.log(`deleted ${deleteID}`);
+	console.log(result);
+	refreshMovies();
+});
+});
+
+	$('#addOptionBtn').click(function () {
+	$('#addPictureOption').addClass('hideOption');
+	$('#addMovieForm').toggleClass('hideOption');
+	$('#deleteMovieOption').addClass('hideOption');
+	$('#editMovieOption').addClass('hideOption');
+});
+
+	$('#editOptionBtn').click(function () {
+	$('#addMovieForm').addClass('hideOption');
+	$('#addPictureOption').addClass('hideOption');
+	$('#editMovieOption').toggleClass('hideOption');
+	$('#deleteMovieOption').addClass('hideOption');
+});
+
+	$('#refreshOptionBtn').click(function () {
+	$('#addPictureOption').addClass('hideOption');
+	$('#addMovieForm').addClass('hideOption');
+	$('#deleteMovieOption').addClass('hideOption');
+	$('#editMovieOption').addClass('hideOption');
+	refreshMovies();
+});
 
 
-// fetch(G_Url)
-// 	.then(res => res.json())
-// 	.then(data => {
-// 		console.log(data)
-//
-//      //    movies.innerHTML = `<div>
-//      //     <h2>${res[0]}</h2>
-//      // </div>`})
+	$('#searchMovieBtn').click(function () {
+	let searchInput = $('#searchMovieInput').val();
+	console.log("search query is: " + searchInput);
+	const myPromise = fetch(`https://dandy-plucky-cover.glitch.me/movies`).then(firstPull => firstPull.json().then(secondPull => {
+	$('#movieDisplay').html('');
+	secondPull.forEach(singlePull => {
+	if (singlePull.title === searchInput) {
+	console.log("yes");
+	console.log(singlePull);
+	$('#movieDisplay').append(`${createImageFromGlitch(singlePull)}`);
+} else {
+	console.log("no");
+}
+});
+}));
+});
 
-		// data.forEach(post => {
-		// 	if (typeof (post.id) === 'number' || typeof (post.id) === 'string') {
-		// 		console.log(post.title);
-        //
-		// 		function hasTitle(){
-		// 		    if(post.title !== 'undefined'){
-		// 		$('#movies').append(
-		// 			`<div class="container">` +
-		// 			`<div class="row" style="outline: auto; padding-bottom: 10px; padding-top: 10px">` +
-		// 			`<div class="col-4">` +
-		// 			`<img src="${post.poster}" style="height:300px; width:200px;">` +
-		// 			`</div>` +
-        //
-		// 			`<div class="col-8">` +
-        //
-		// 			`<h3>${post.title}</h3>` +
-		// 			`<ul style="list-style: none">` +
-		// 			`<li>Genre:    ${post.genre}</li>` +
-		// 			`<li>Rating:  ${post.rating}</li>` +
-		// 			`<li>Director:  ${post.director}</li>` +
-		// 			`<li>Plot:  ${post.plot}</li>` +
-		// 			`</ul>`+
-		// 			`<button id="${post.id}" >delete movie</button>` +
-		// 			`</div>` +
-		// 			`</div>` +
-        //
-		// 			`</div>`
-        //
-		// 		)
-		// 	}
-        //
-		// }}})})
+});
+
+
