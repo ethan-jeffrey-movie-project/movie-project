168bbd9d57da2d46862bbebb62f2fe4991728849
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-12-29 01:37:28 -0600

    commiting progress

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,73 @@
 
-// const userAction = async () => {
+const API_URL = 'https://lucky-melodic-particle.glitch.me/movies'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://lucky-melodic-particle.glitch.me/movies/'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+// async function getInfo() {
 // 	const response = await fetch('https://lucky-melodic-particle.glitch.me/movies');
-// 	const myJson = await response.json(); //extract JSON from the http response
-// 	return myJson// do something with myJson
+// 	let data = await response.json(); //extract JSON from the http response
+// 	return data;// do something with myJson
 // }
 
-async function getInfo() {
-	const response = await fetch('https://lucky-melodic-particle.glitch.me/movies');
-	let data = await response.json(); //extract JSON from the http response
-	return data;// do something with myJson
+// getInfo().then(data => console.log(data[0]["poster_path"]))
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+	const res = await fetch(url)
+	const data = await res.json()
+
+	showMovies(data)
 }
 
-// getInfo().then(data => console.log(data.))
+function showMovies(movies) {
+	main.innerHTML = ''
 
+	movies.forEach((movie) => {
+		const { title, poster_path, vote_average, overview } = movie
 
-// const userAction = async () => {
-// 	const response = await fetch('http://example.com/movies.json', {
-// 		method: 'POST',
-// 		body: myBody, // string or object
-// 		headers: {
-// 			'Content-Type': 'application/json'
-// 		}
-// 	});
-// 	const myJson = await response.json(); //extract JSON from the http response
-// 	// do something with myJson
-// }
+		const movieEl = document.createElement('div')
+		movieEl.classList.add('movie')
 
+		movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+		main.appendChild(movieEl)
+	})
+}
+
+function getClassByRate(vote) {
+	if(vote >= 8) {
+		return 'green'
+	} else if(vote >= 5) {
+		return 'orange'
+	} else {
+		return 'red'
+	}
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	const searchTerm = search.value
+
+	if(searchTerm && searchTerm !== '') {
+		getMovies(SEARCH_API + searchTerm)
+
+		search.value = ''
+	} else {
+		window.location.reload()
+	}
+})
