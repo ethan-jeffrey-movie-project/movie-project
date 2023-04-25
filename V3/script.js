fafa79bfc2e1a384ce9df0f050e4085c0a534cff
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-04-24 21:55:32 -0500

    pulled and appended data to some cards

--- V3/script.js
+++ V3/script.js
@@ -1,39 +1,91 @@
-import {TMDB_key} from "../keys.js";
-
-import {navbar} from "./navbar.js";
+import { TMDB_key } from "../keys.js";
+import { navbar } from "./navbar.js";
+// import { populateCards } from "./populateCards.js";
 
 const movie_key = TMDB_key;
 
-navigator.geolocation.getCurrentPosition(function(position) {
-	var lat = position.coords.latitude;
-	var lng = position.coords.longitude;
+function loadHorrorMovies(swiperId) {
+	navigator.geolocation.getCurrentPosition(function (position) {
+		const lat = position.coords.latitude;
+		const lng = position.coords.longitude;
 
-	// build URL with API parameters
-	const url = `https://api.themoviedb.org/3/discover/movie/?api_key=${movie_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_release_type=3|2&release_date.lte=now&with_original_language=en&page=1&primary_release_date.lte=now&primary_release_date.gte=now-7d&timezone=America/New_York&with_watch_monetization_types=flatrate&with_watch_providers=8&watch_region=US&watch_provider=8&with_companies=174&with_crew=2654,2557&with_cast=16828,1626610&with_theaters=true&with_movie_discovery=true&with_release_type.lte=3&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
+		const url = `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=27&with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2&with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
 
-	// const url = `https://api.themoviedb.org/3/movie/popular?api_key=${movie_key}&language=en-US&page=1`;
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				const movies = data.results.filter((movie) => movie.genre_ids.includes(27));
+				const swiperWrapper = document.querySelector(`#${swiperId} .swiper-wrapper`);
+				swiperWrapper.innerHTML = "";
 
-	// make a fetch request to the TMDB API
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			// handle successful response from TMDB API
-			console.log(data);
-			console.log(lat);
-			console.log(lng);
-		})
-		.catch(error => {
-			// handle error response from TMDB API
-			console.log(error);
-		});
-}, function(error) {
-	// handle error retrieving user's location
-	console.log(error);
-});
+				let slideIndex = -1;
+				movies.forEach((movie, index) => {
+					if (index % 4 === 0) {
+						slideIndex++;
+						const slide = document.createElement("div");
+						slide.classList.add("swiper-slide");
+						swiperWrapper.appendChild(slide);
+					}
+
+					const slide = swiperWrapper.children[slideIndex];
+
+					const card = document.createElement("div");
+					card.classList.add("card");
+
+					const img = document.createElement("img");
+					img.classList.add("card-img-top");
+					img.src = `https://image.tmdb.org/t/p/w154/${movie.poster_path}`;
+					img.alt = movie.original_title;
+					card.appendChild(img);
+
+					const cardBody = document.createElement("div");
+					cardBody.classList.add("card-body");
+
+					const title = document.createElement("h5");
+					title.classList.add("card-title");
+					title.textContent = movie.original_title;
+					cardBody.appendChild(title);
+
+					const description = document.createElement("p");
+					description.classList.add("card-text");
+					description.textContent = movie.overview;
+					cardBody.appendChild(description);
+
+					const button = document.createElement("a");
+					button.classList.add("btn", "btn-primary");
+					button.href = "#";
+					button.textContent = "Add to Favorites";
+					cardBody.appendChild(button);
+
+					card.appendChild(cardBody);
+					slide.appendChild(card);
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, function (error) {
+		console.log(error);
+	});
+
+	var swiper = new Swiper(`#${swiperId}`, {
+		direction: "horizontal",
+	});
+
+	var actionSwiper = new Swiper("#action-swiper", {
+		direction: "horizontal",
+	});
+
+	swiper.on("reachEnd", function () {
+		actionSwiper.slideTo(0);
+	});
+
+	actionSwiper.on("reachEnd", function () {
+		swiper.slideTo(0);
+	});
+
+	document.querySelector("#navbar").innerHTML = navbar;
+}
 
 
-
-var swiper = new Swiper(".mySwiper", {});
-
-
-document.querySelector('#navbar').innerHTML = navbar;
+loadHorrorMovies("horror-swiper");
