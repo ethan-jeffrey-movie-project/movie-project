7c96b011e19ed0350231839969a808bbf8198085
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-05-05 02:34:32 -0500

    function displays cards based on their genreId

--- V3/fetchMovies.js
+++ V3/fetchMovies.js
@@ -1,7 +1,25 @@
+/*
+
+5-5-23
+1:33am
+
+ this function fetches all the movies depending on their genreId in the array
+ the cards need to be stylized so there can be multiple movie cards showing
+
+* */
+
+
+
+// import { navbar } from "./navbar.js";
+// import {TMDB_key} from "../keys.js";
+//
+// import { populateCards } from "./populateCards.js";// comment this out
+//
+// const movie_key = TMDB_key;
 //
 //
-// const genres = [
-// 	{ id: 28, name: "Action" },
+// export const genres = [
+// 	{ id: 27, name: "Action" },
 // 	{ id: 12, name: "Adventure" },
 // 	{ id: 16, name: "Animation" },
 // 	{ id: 35, name: "Comedy" },
@@ -22,18 +40,98 @@
 // 	{ id: 37, name: "Western" }
 // ];
 //
-// async function fetchGenres() {
-// 	const promises = genres.map((genre) => {
-// 		return fetch(
-// 			`https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre.id}`
-// 		).then((response) => response.json());
-// 	});
+// export async function fetchMoviesByGenre(genre) {
+// 	const url = `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US
+//   &sort_by=popularity.desc
+//   &include_adult=false
+//   &include_video=false
+//   &page=1
+//   &with_genres=${genre}`;
 //
-// 	const results = await Promise.all(promises);
-// 	console.log(results);
+// 	const response = await fetch(url);
+// 	const data = await response.json();
+// 	return data.results;
 // }
 //
-// fetchGenres();
 //
+// export async function loadMoviesByGenres() {
+// 	// iterate over the genres array
+// 	for (let i = 0; i < genres.length; i++) {
+// 		const genre = genres[i];
+// 		const movies = await fetchMoviesByGenre(genre.id);
+// 		let isModified = movies.length > 0;
 //
+// 		// display movies for this genre
+// 		const swiperId = `#${genre.name.toLowerCase()}-swiper`;
+// 		const swiperWrapper = document.querySelector(`${swiperId} .swiper-wrapper`);
+// 		swiperWrapper.innerHTML = "";
 //
+// 		let slideIndex = -1;
+// 		movies.forEach((movie, index) => {
+// 			// Create a new slide every 5th movie
+// 			if (index % 5 === 0) {
+// 				slideIndex++;
+// 				const slide = document.createElement("div");
+// 				slide.classList.add("swiper-slide");
+// 				swiperWrapper.appendChild(slide);
+// 				slide.classList.add("swiper-slide");
+// 				slide.classList.add("justify-content-between");
+// 				slide.classList.add("d-flex");
+// 			}
+//
+// 			const slide = swiperWrapper.children[slideIndex];
+//
+// 			// Create a new card for the movie
+// 			const card = document.createElement("div");
+// 			card.classList.add("card");
+// 			card.style.maxWidth = "19%";
+//
+// 			const img = document.createElement("img");
+// 			img.classList.add("card-img-top");
+// 			img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+// 			img.style.maxHeight = "90%";
+// 			img.style.maxWidth = "150px";
+// 			img.alt = movie.original_title;
+// 			card.appendChild(img);
+//
+// 			const cardBody = document.createElement("div");
+// 			cardBody.classList.add("card-body");
+//
+// 			const title = document.createElement("h5");
+// 			title.classList.add("card-title");
+// 			title.textContent = movie.original_title;
+// 			cardBody.appendChild(title);
+//
+// 			const description = document.createElement("p");
+// 			description.classList.add("card-text");
+// 			description.textContent = movie.overview;
+// 			cardBody.appendChild(description);
+//
+// 			const button = document.createElement("a");
+// 			button.classList.add("btn", "btn-primary");
+// 			button.href = "#";
+// 			button.textContent = "Add to Favorites";
+// 			cardBody.appendChild(button);
+//
+// 			card.appendChild(cardBody);
+// 			slide.appendChild(card);
+// 		});
+//
+// 		// Initialize the Swiper instances for this genre
+// 		var swiper = new Swiper(swiperId, {
+// 			direction: "horizontal",
+// 		});
+//
+// 		// Add the current genre swiper to genreSwiper array
+// 		genreSwiper.push(swiper);
+//
+// 		// Set up the "reachEnd" event listeners for the Swiper instances
+// 		swiper.on("reachEnd", function () {
+// 			const nextSwiperIndex = (i + 1) % genres.length;
+// 			genreSwiper[nextSwiperIndex].slideTo(0);
+// 		});
+// 	}
+//
+// 	document.querySelector("#navbar").innerHTML = navbar;
+// }
+
