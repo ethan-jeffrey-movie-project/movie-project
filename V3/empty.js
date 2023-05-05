1247a6c8e4eab6eca3959f74832dcf5ce1bce802
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-05-05 03:34:14 -0500

    function on empty.js creates cards with 2 genres

---
+++ V3/empty.js
@@ -0,0 +1,184 @@
+
+import { navbar } from "./navbar.js";
+import {TMDB_key} from "../keys.js";
+
+const movie_key = TMDB_key;
+
+function loadHorrorAndActionMovies(horrorSwiperId, actionSwiperId) {
+	// Use the Geolocation API to get the user's current position
+	navigator.geolocation.getCurrentPosition(function (position) {
+		const lat = position.coords.latitude;
+		const lng = position.coords.longitude;
+
+		// Construct the URL for the API request for horror movies
+		const horrorUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US
+      &sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=27
+      &with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2
+      &with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
+
+		// Construct the URL for the API request for action movies
+		const actionUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US
+      &sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28
+      &with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2
+      &with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
+
+		// Make the API requests and handle the responses
+		Promise.all([
+			fetch(horrorUrl).then((response) => response.json()),
+			fetch(actionUrl).then((response) => response.json()),
+		])
+			.then(([horrorData, actionData]) => {
+				// Filter the responses to get only horror and action movies
+				const horrorMovies = horrorData.results.filter((movie) => movie.genre_ids.includes(27));
+				const actionMovies = actionData.results.filter((movie) => movie.genre_ids.includes(28));
+
+				// Populate the horror movies container
+				const horrorSwiperWrapper = document.querySelector(`#${horrorSwiperId} .swiper-wrapper`);
+				horrorSwiperWrapper.innerHTML = "";
+				let slideIndex = -1;
+				horrorMovies.forEach((movie, index) => {
+					// Create a new slide every 5th movie
+					if (index % 5 === 0) {
+						slideIndex++;
+						const slide = document.createElement("div");
+						slide.classList.add("swiper-slide");
+						horrorSwiperWrapper.appendChild(slide);
+						slide.classList.add("swiper-slide");
+						slide.classList.add("justify-content-between");
+						slide.classList.add("d-flex");
+					}
+
+					const slide = horrorSwiperWrapper.children[slideIndex];
+
+					// Create a new card for the movie
+					const card = document.createElement("div");
+					card.classList.add("card");
+					card.style.maxWidth = "19%"
+
+					const img = document.createElement("img");
+					img.classList.add("card-img-top");
+					img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+					img.style.maxHeight = "90%"
+					img.style.maxWidth = "150px"
+					img.alt = movie.original_title;
+					card.appendChild(img);
+
+					const cardBody = document.createElement("div");
+					cardBody.classList.add("card-body");
+
+					const title = document.createElement("h5");
+					title.classList.add("card-title");
+					title.textContent = movie.original_title;
+					cardBody.appendChild(title);
+
+					const description = document.createElement("p");
+					description.classList.add("card-text");
+					description.textContent = movie.overview;
+					cardBody.appendChild(description);
+
+					// Add a different class to the card depending on its genre
+					if (movie.genre_ids.includes(27)) {
+						card.classList.add("horror-movie");
+					} else if (movie.genre_ids.includes(28)) {
+						card.classList.add("action-movie");
+					}
+
+					const button = document.createElement("a");
+					button.classList.add("btn", "btn-primary");
+					button.href = "#";
+					button.textContent = "Add to Favorites";
+					cardBody.appendChild(button);
+
+					card.appendChild(cardBody);
+					slide.appendChild(card);
+				});
+
+				// Populate the action movies container
+				const actionSwiperWrapper = document.querySelector(`#${actionSwiperId} .swiper-wrapper`);
+				actionSwiperWrapper.innerHTML = "";
+				slideIndex = -1;
+				actionMovies.forEach((movie, index) => {
+					// Create a new slide every 5th movie
+					if (index % 5 === 0) {
+						slideIndex++;
+						const slide = document.createElement("div");
+						slide.classList.add("swiper-slide");
+						actionSwiperWrapper.appendChild(slide);
+						slide.classList.add("swiper-slide");
+						slide.classList.add("justify-content-between");
+						slide.classList.add("d-flex");
+					}
+
+					const slide = actionSwiperWrapper.children[slideIndex];
+
+					// Create a new card for the movie
+					const card = document.createElement("div");
+					card.classList.add("card");
+					card.style.maxWidth = "19%"
+
+					const img = document.createElement("img");
+					img.classList.add("card-img-top");
+					img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+					img.style.maxHeight = "90%"
+					img.style.maxWidth = "150px"
+					img.alt = movie.original_title;
+					card.appendChild(img);
+
+					const cardBody = document.createElement("div");
+					cardBody.classList.add("card-body");
+
+					const title = document.createElement("h5");
+					title.classList.add("card-title");
+					title.textContent = movie.original_title;
+					cardBody.appendChild(title);
+
+					const description = document.createElement("p");
+					description.classList.add("card-text");
+					description.textContent = movie.overview;
+					cardBody.appendChild(description);
+
+					// Add a different class to the card depending on its genre
+					if (movie.genre_ids.includes(27)) {
+						card.classList.add("horror-movie");
+					} else if (movie.genre_ids.includes(28)) {
+						card.classList.add("action-movie");
+					}
+
+					const button = document.createElement("a");
+					button.classList.add("btn", "btn-primary");
+					button.href = "#";
+					button.textContent = "Add to Favorites";
+					cardBody.appendChild(button);
+
+					card.appendChild(cardBody);
+					slide.appendChild(card);
+				});
+
+				// Initialize the Swiper instances
+				const swiper = new Swiper(`#${horrorSwiperId}`, {
+					direction: "horizontal",
+				});
+
+				const actionSwiper = new Swiper(`#${actionSwiperId}`, {
+					direction: "horizontal",
+				});
+
+				swiper.on("reachEnd", function () {
+					actionSwiper.slideTo(0);
+				});
+
+				actionSwiper.on("reachEnd", function () {
+					swiper.slideTo(0);
+				});
+
+				document.querySelector("#navbar").innerHTML = navbar;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, function (error) {
+		console.log(error);
+	});
+}
+
+loadHorrorAndActionMovies("horror-swiper", "action-swiper");
--- V3/script.js
+++ V3/script.js
@@ -11,14 +11,19 @@ function loadHorrorMovies(swiperId) {
 		const lng = position.coords.longitude;
 
 		// Construct the URL for the API request
-		const url = `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=27&with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2&with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
+		const url = `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US
+	&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=27,35
+	&with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2
+	&with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
+
 
 		// Make the API request and handle the response
 		fetch(url)
 			.then((response) => response.json())
 			.then((data) => {
 				// Filter the response to get only horror movies
-				const movies = data.results.filter((movie) => movie.genre_ids.includes(27));
+				const movies = data.results.filter((movie) => movie.genre_ids.includes(27) || movie.genre_ids.includes(35));
+
 				const swiperWrapper = document.querySelector(`#${swiperId} .swiper-wrapper`);
 				swiperWrapper.innerHTML = "";
 
@@ -101,45 +106,47 @@ function loadHorrorMovies(swiperId) {
 	document.querySelector("#navbar").innerHTML = navbar;
 }
 
-const genres = [
-	{ id: 28, name: "Action" },
-	{ id: 12, name: "Adventure" },
-	{ id: 16, name: "Animation" },
-	{ id: 35, name: "Comedy" },
-	{ id: 80, name: "Crime" },
-	{ id: 99, name: "Documentary" },
-	{ id: 18, name: "Drama" },
-	{ id: 10751, name: "Family" },
-	{ id: 14, name: "Fantasy" },
-	{ id: 36, name: "History" },
-	{ id: 27, name: "Horror" },
-	{ id: 10402, name: "Music" },
-	{ id: 9648, name: "Mystery" },
-	{ id: 10749, name: "Romance" },
-	{ id: 878, name: "Science Fiction" },
-	{ id: 10770, name: "TV Movie" },
-	{ id: 53, name: "Thriller" },
-	{ id: 10752, name: "War" },
-	{ id: 37, name: "Western" }
-];
-
- function fetchGenres() {
-	const promises = genres.map((genre) => {
-		return fetch(
-			`https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre.id}`
-		).then((response) => response.json());
-	});
-
-	const results =  Promise.all(promises);
-	console.log(results);
-}
-
-fetchGenres();
-
-
-
-
 loadHorrorMovies("horror-swiper");
+
+// const genres = [
+// 	{ id: 28, name: "Action" },
+// 	{ id: 12, name: "Adventure" },
+// 	{ id: 16, name: "Animation" },
+// 	{ id: 35, name: "Comedy" },
+// 	{ id: 80, name: "Crime" },
+// 	{ id: 99, name: "Documentary" },
+// 	{ id: 18, name: "Drama" },
+// 	{ id: 10751, name: "Family" },
+// 	{ id: 14, name: "Fantasy" },
+// 	{ id: 36, name: "History" },
+// 	{ id: 27, name: "Horror" },
+// 	{ id: 10402, name: "Music" },
+// 	{ id: 9648, name: "Mystery" },
+// 	{ id: 10749, name: "Romance" },
+// 	{ id: 878, name: "Science Fiction" },
+// 	{ id: 10770, name: "TV Movie" },
+// 	{ id: 53, name: "Thriller" },
+// 	{ id: 10752, name: "War" },
+// 	{ id: 37, name: "Western" }
+// ];
+//
+//  function fetchGenres() {
+// 	const promises = genres.map((genre) => {
+// 		return fetch(
+// 			`https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre.id}`
+// 		).then((response) => response.json());
+// 	});
+//
+// 	const results =  Promise.all(promises);
+// 	console.log(results);
+// }
+//
+// fetchGenres();
+
+
+
+
+
 
 
 
