d029bc525a42d47b1081d9dd7fad8e8128f92967
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-05-07 23:12:53 -0500

    functions fetch and displays all 19 genres

--- V3/empty.js
+++ V3/empty.js
@@ -1,79 +1,56 @@
 
 /*
 
-5-5-23
-15:40
+5-7-23
+2311
 
- this function fetches 2 movie genres and creates cards
- populateMovies function was made to shorten loadHorrorAndActionMovies function
 
 * */
+
+
+
 
 import { navbar } from "./navbar.js";
 import { TMDB_key } from "../keys.js";
 
 const movie_key = TMDB_key;
 
-function loadHorrorAndActionMovies(horrorSwiperId, actionSwiperId) {
+function loadHorrorAndActionMovies(swiperIds, genreIds) {
 	// Use the Geolocation API to get the user's current position
 	navigator.geolocation.getCurrentPosition(
 		function (position) {
 			const lat = position.coords.latitude;
 			const lng = position.coords.longitude;
 
-			// Construct the URL for the API request for horror movies
-			const horrorUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US
-      &sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=27
-      &with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2
-      &with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
-
-			// Construct the URL for the API request for action movies
-			const actionUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US
-      &sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28
-      &with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2
-      &with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
+			// Construct the URLs for the API requests for the desired genres
+			const genreUrls = genreIds.map((genreId) => {
+				return `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US
+          			&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genreId}
+          			&with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2
+          			&with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
+			});
 
 			// Make the API requests and handle the responses
-			Promise.all([
-				fetch(horrorUrl).then((response) => response.json()),
-				fetch(actionUrl).then((response) => response.json()),
-			])
-				.then(([horrorData, actionData]) => {
-					// Filter the responses to get only horror and action movies
-					const horrorMovies = horrorData.results.filter((movie) =>
-						movie.genre_ids.includes(27)
-					);
-					const actionMovies = actionData.results.filter((movie) =>
-						movie.genre_ids.includes(28)
-					);
-
-					// Populate the horror movies container
-					const horrorSwiperWrapper = document.querySelector(
-						`#${horrorSwiperId} .swiper-wrapper`
-					);
-					populateMovies(horrorSwiperWrapper, horrorMovies);
-
-					// Populate the action movies container
-					const actionSwiperWrapper = document.querySelector(
-						`#${actionSwiperId} .swiper-wrapper`
-					);
-					populateMovies(actionSwiperWrapper, actionMovies);
-
-					// Initialize the Swiper instances
-					const swiper = new Swiper(`#${horrorSwiperId}`, {
-						direction: "horizontal",
+			Promise.all(genreUrls.map((url) => fetch(url).then((response) => response.json())))
+				.then((responses) => {
+					// Filter the responses to get the movies for each genre
+					const genreMovies = responses.map((response, index) => {
+						return response.results.filter((movie) => movie.genre_ids.includes(genreIds[index]));
 					});
 
-					const actionSwiper = new Swiper(`#${actionSwiperId}`, {
-						direction: "horizontal",
-					});
+					// Populate the swiper containers for each genre
+					swiperIds.forEach((swiperId, index) => {
+						const genreSwiperWrapper = document.querySelector(`#${swiperId} .swiper-wrapper`);
+						populateMovies(genreSwiperWrapper, genreMovies[index], genreIds[index]);
 
-					swiper.on("reachEnd", function () {
-						actionSwiper.slideTo(0);
-					});
+						// Initialize the Swiper instances
+						const swiper = new Swiper(`#${swiperId}`, {
+							direction: "horizontal",
+						});
 
-					actionSwiper.on("reachEnd", function () {
-						swiper.slideTo(0);
+						swiper.on("reachEnd", function () {
+							swiper.slideTo(0);
+						});
 					});
 
 					document.querySelector("#navbar").innerHTML = navbar;
@@ -88,9 +65,36 @@ function loadHorrorAndActionMovies(horrorSwiperId, actionSwiperId) {
 	);
 }
 
-function populateMovies(container, movies, genreClass) {
+const genres = [
+	{ id: 28, name: "Action" },
+	{ id: 12, name: "Adventure" },
+	{ id: 16, name: "Animation" },
+	{ id: 35, name: "Comedy" },
+	{ id: 80, name: "Crime" },
+	{ id: 99, name: "Documentary" },
+	{ id: 18, name: "Drama" },
+	{ id: 10751, name: "Family" },
+	{ id: 14, name: "Fantasy" },
+	{ id: 36, name: "History" },
+	{ id: 27, name: "Horror" },
+	{ id: 10402, name: "Music" },
+	{ id: 9648, name: "Mystery" },
+	{ id: 10749, name: "Romance" },
+	{ id: 878, name: "Science-Fiction" },
+	{ id: 10770, name: "TV-Movie" },
+	{ id: 53, name: "Thriller" },
+	{ id: 10752, name: "War" },
+	{ id: 37, name: "Western" }
+];
+
+const swiperIds = genres.map((genre) => `${genre.name.toLowerCase()}-swiper`);
+const genreIds = genres.map((genre) => genre.id);
+
+function populateMovies(container, movies, genreId) {
 	container.innerHTML = "";
 	let slideIndex = -1;
+
+	const genre = genres.find((g) => g.id === genreId);
 
 	movies.forEach((movie, index) => {
 		// Create a new slide every 5th movie
@@ -133,12 +137,8 @@ function populateMovies(container, movies, genreClass) {
 		cardBody.appendChild(description);
 
 		// Add a different class to the card depending on its genre
-		if (movie.genre_ids.includes(27)) {
-			card.classList.add("horror-movie");
-		} else if (movie.genre_ids.includes(28)) {
-			card.classList.add("action-movie");
-		} else if (genreClass) {
-			card.classList.add(genreClass);
+		if (movie.genre_ids.includes(genreId)) {
+			card.classList.add(`${genre.name.toLowerCase()}-swiper`);
 		}
 
 		const button = document.createElement("a");
@@ -152,4 +152,4 @@ function populateMovies(container, movies, genreClass) {
 	});
 }
 
-loadHorrorAndActionMovies("horror-swiper", "action-swiper");
+loadHorrorAndActionMovies(swiperIds,genreIds)
--- V3/oldFunction.js
+++ V3/oldFunction.js
@@ -1,88 +1,143 @@
+//
+//
 // import { navbar } from "./navbar.js";
-// import {TMDB_key} from "../keys.js";
-// // import { populateCards } from "./populateCards.js";
+// import { TMDB_key } from "../keys.js";
 //
 // const movie_key = TMDB_key;
 //
-// function loadHorrorMovies(swiperId, page = 1) {
-// 	navigator.geolocation.getCurrentPosition(function (position) {
-// 		const lat = position.coords.latitude;
-// 		const lng = position.coords.longitude;
+// function loadMoviesByGenres(swiperIds, genreIds) {
+// 	// Use the Geolocation API to get the user's current position
+// 	navigator.geolocation.getCurrentPosition(
+// 		function (position) {
+// 			const lat = position.coords.latitude;
+// 			const lng = position.coords.longitude;
 //
-// 		const url = `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=27&with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2&with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
-//
-// 		fetch(url)
-// 			.then((response) => response.json())
-// 			.then((data) => {
-// 				const movies = data.results.filter((movie) => movie.genre_ids.includes(27));
-//
-// 				movies.forEach((movie) => {
-// 					const card = createMovieCard(movie);
-// 					const swiperWrapper = document.querySelector(`#${swiperId} .swiper-wrapper`);
-// 					swiperWrapper.appendChild(card);
-// 				});
-//
-// 				var swiper = new Swiper(`#${swiperId}`, {
-// 					direction: "horizontal",
-// 				});
-//
-// 				swiper.on("reachEnd", function () {
-// 					loadHorrorMovies(swiperId, page + 1);
-// 				});
-//
-// 				document.querySelector("#navbar").innerHTML = navbar;
-// 			})
-// 			.catch((error) => {
-// 				console.log(error);
+// 			// Construct the URLs for the API requests for the desired genres
+// 			const genreUrls = genreIds.map((genreId) => {
+// 				return `https://api.themoviedb.org/3/discover/movie?api_key=${movie_key}&language=en-US
+//           			&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genreId}
+//           			&with_watch_monetization_types=flatrate&with_release_type.lte=3&with_release_type.gte=2
+//           			&with_theaters=true&with_movie_discovery=true&with_theaters.lat=${lat}&with_theaters.lng=${lng}&with_theaters.radius=100km`;
 // 			});
-// 	}, function (error) {
-// 		console.log(error);
+//
+// 			// Make the API requests and handle the responses
+// 			Promise.all(genreUrls.map((url) => fetch(url).then((response) => response.json())))
+// 				.then((responses) => {
+// 					// Filter the responses to get the movies for each genre
+// 					const genreMovies = responses.map((response) => {
+// 						return response.results.filter((movie) => movie.genre_ids.includes(genreId));
+// 					});
+//
+// 					// Populate the swiper containers for each genre
+// 					swiperIds.forEach((swiperId, index) => {
+// 						const genreSwiperWrapper = document.querySelector(`#${swiperId} .swiper-wrapper`);
+// 						populateMovies(genreSwiperWrapper, genreMovies[index], `genre-${genreIds[index]}`);
+//
+// 						// Initialize the Swiper instances
+// 						const swiper = new Swiper(`#${swiperId}`, {
+// 							direction: "horizontal",
+// 						});
+//
+// 						swiper.on("reachEnd", function () {
+// 							swiper.slideTo(0);
+// 						});
+// 					});
+//
+// 					document.querySelector("#navbar").innerHTML = navbar;
+// 				})
+// 				.catch((error) => {
+// 					console.log(error);
+// 				});
+// 		},
+// 		function (error) {
+// 			console.log(error);
+// 		}
+// 	);
+// }
+//
+// const genres = [
+// 	{ id: 28, name: "Action" },
+// 	{ id: 12, name: "Adventure" },
+// 	{ id: 16, name: "Animation" },
+// 	{ id: 35, name: "Comedy" },
+// 	{ id: 80, name: "Crime" },
+// 	{ id: 99, name: "Documentary" },
+// 	{ id: 18, name: "Drama" },
+// 	{ id: 10751, name: "Family" },
+// 	{ id: 14, name: "Fantasy" },
+// 	{ id: 36, name: "History" },
+// 	{ id: 27, name: "Horror" },
+// 	{ id: 10402, name: "Music" },
+// 	{ id: 9648, name: "Mystery" },
+// 	{ id: 10749, name: "Romance" },
+// 	{ id: 878, name: "Science Fiction" },
+// 	{ id: 10770, name: "TV Movie" },
+// 	{ id: 53, name: "Thriller" },
+// 	{ id: 10752, name: "War" },
+// 	{ id: 37, name: "Western" }
+// ];
+//
+//
+// function populateMovies(container, movies, genreId) {
+// 	container.innerHTML = "";
+// 	let slideIndex = -1;
+//
+// 	const genre = genres.find((g) => g.id === genreId);
+//
+// 	movies.forEach((movie, index) => {
+// 		// Create a new slide every 5th movie
+// 		if (index % 5 === 0) {
+// 			slideIndex++;
+// 			const slide = document.createElement("div");
+// 			slide.classList.add("swiper-slide");
+// 			container.appendChild(slide);
+// 			slide.classList.add("swiper-slide");
+// 			slide.classList.add("justify-content-between");
+// 			slide.classList.add("d-flex");
+// 		}
+//
+// 		const slide = container.children[slideIndex];
+//
+// 		// Create a new card for the movie
+// 		const card = document.createElement("div");
+// 		card.classList.add("card");
+// 		card.style.maxWidth = "19%";
+//
+// 		const img = document.createElement("img");
+// 		img.classList.add("card-img-top");
+// 		img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+// 		img.style.maxHeight = "90%";
+// 		img.style.maxWidth = "150px";
+// 		img.alt = movie.original_title;
+// 		card.appendChild(img);
+//
+// 		const cardBody = document.createElement("div");
+// 		cardBody.classList.add("card-body");
+//
+// 		const title = document.createElement("h5");
+// 		title.classList.add("card-title");
+// 		title.textContent = movie.original_title;
+// 		cardBody.appendChild(title);
+//
+// 		const description = document.createElement("p");
+// 		description.classList.add("card-text");
+// 		description.textContent = movie.overview;
+// 		cardBody.appendChild(description);
+//
+// 		// Add a different class to the card depending on its genre
+// 		if (movie.genre_ids.includes(genreId)) {
+// 			card.classList.add(`${genre.name.toLowerCase()}-movie`);
+// 		}
+//
+// 		const button = document.createElement("a");
+// 		button.classList.add("btn", "btn-primary");
+// 		button.href = "#";
+// 		button.textContent = "Add to Favorites";
+// 		cardBody.appendChild(button);
+//
+// 		card.appendChild(cardBody);
+// 		slide.appendChild(card);
 // 	});
 // }
-// // loadHorrorMovies("horror-Swiper",1)
-// function createMovieCard(movie) {
-// 	const card = document.createElement("div");
-// 	card.classList.add("card");
-// 	card.style.maxWidth = "19%";
 //
-// 	const img = document.createElement("img");
-// 	img.classList.add("card-img-top");
-// 	img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-// 	img.style.maxHeight = "90%";
-// 	img.style.maxWidth = "150px";
-// 	img.alt = movie.original_title;
-// 	card.appendChild(img);
-//
-// 	const cardBody = document.createElement("div");
-// 	cardBody.classList.add("card-body");
-//
-// 	const title = document.createElement("h5");
-// 	title.classList.add("card-title");
-// 	title.textContent = movie.original_title;
-// 	cardBody.appendChild(title);
-//
-// 	const description = document.createElement("p");
-// 	description.classList.add("card-text");
-// 	description.textContent = movie.overview;
-// 	cardBody.appendChild(description);
-//
-// 	const button = document.createElement("a");
-// 	button.classList.add("btn", "btn-primary");
-// 	button.href = "#";
-// 	button.textContent = "Add to Favorites";
-// 	cardBody.appendChild(button);
-//
-// 	card.appendChild(cardBody);
-//
-// 	const slide = document.createElement("div");
-// 	slide.classList.add("swiper-slide", "justify-content-between", "d-flex");
-// 	slide.appendChild(card);
-//
-// 	return slide;
-// }
-//
-//
-// loadHorrorMovies("horror-swiper",4);
-//
-//
-//
+// loadMoviesByGenres("action-swiper", "28")
