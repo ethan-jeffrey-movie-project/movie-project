c5499c75e6ddfb6af800256a4f8bf21e0bd8a88b
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-06-02 22:54:24 -0500

    refactored getsavedmovies function

--- V3/_fetchMovies.js
+++ V3/_fetchMovies.js
@@ -134,3 +134,86 @@
 // 	document.querySelector("#navbar").innerHTML = navbar;
 // }
 
+function displaySavedMovies() {
+	fetch("https://vivid-flawless-almandine.glitch.me/movies")
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			const swiperWrapper = document.querySelector("#saved-swiper .swiper-wrapper");
+
+			// Clear the swiper wrapper
+			swiperWrapper.innerHTML = "";
+
+			// Determine the number of slides required
+			const numSlides = Math.ceil(data.length / 4);
+
+			for (let i = 0; i < numSlides; i++) {
+				// Create a swiper slide container
+				const swiperSlide = document.createElement("div");
+				swiperSlide.classList.add("swiper-slide", "justify-content-between", "d-flex");
+
+				// Calculate the range of movies to display on this slide
+				const startIndex = i * 4;
+				const endIndex = Math.min(startIndex + 4, data.length);
+
+				// Iterate over the movies and create card-swipers for this slide
+				for (let j = startIndex; j < endIndex; j++) {
+					const movie = data[j];
+
+					const cardSwiper = document.createElement("div");
+					cardSwiper.classList.add("card-swiper");
+					cardSwiper.style.maxWidth = "19%";
+
+					const img = document.createElement("img");
+					img.style.height = "100%";
+					img.style.width = "100%";
+					img.alt = movie.original_title;
+					if (movie.backdrop_path) {
+						img.src = `https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`;
+					} else {
+						img.src = `https://images.pexels.com/photos/2873486/pexels-photo-2873486.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2`;
+					}
+					cardSwiper.appendChild(img);
+
+					const cardBody = document.createElement("div");
+					cardBody.classList.add("p-3", "card-body");
+
+					const title = document.createElement("h5");
+					title.classList.add("card-title");
+					title.textContent = movie.original_title;
+					cardBody.appendChild(title);
+
+					const description = document.createElement("p");
+					description.classList.add("card-text");
+					description.textContent = movie.overview;
+					cardBody.appendChild(description);
+
+					const deleteButton = document.createElement("a");
+					deleteButton.classList.add("btn", "btn-danger");
+					deleteButton.href = "#";
+					deleteButton.textContent = "delete";
+					deleteButton.addEventListener("click", () => deleteMovie(movie.id));
+					cardBody.appendChild(deleteButton);
+
+					const modifyButton = document.createElement("a");
+					modifyButton.classList.add("btn", "btn-primary");
+					modifyButton.href = "#";
+					modifyButton.textContent = "modify";
+					modifyButton.addEventListener("click", () => modifyMovie(movie.id));
+					cardBody.appendChild(modifyButton);
+
+					cardSwiper.appendChild(cardBody);
+					swiperSlide.appendChild(cardSwiper);
+				}
+
+				// Append the swiper slide to the swiper wrapper
+				swiperWrapper.appendChild(swiperSlide);
+			}
+
+			// Initialize the Swiper instance
+			const savedSwiper = new Swiper("#saved-swiper", {
+				// Swiper configuration options
+			});
+		})
+		.catch(error => console.log(error));
+}
