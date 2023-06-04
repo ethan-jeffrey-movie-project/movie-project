0970e16292ae1aa147eb62f87c119dfc27c96102
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-06-04 18:54:51 -0500

    added link for genres

--- V3/navbar.js
+++ V3/navbar.js
@@ -10,7 +10,7 @@ export const navbar = `
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="overflow-x: auto; white-space: nowrap">
+    <div class="collapse navbar-collapse " id="navbarSupportedContent" style="overflow-x: auto; white-space: nowrap; max-height:5vh;">
     
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
@@ -22,67 +22,68 @@ export const navbar = `
         
       </ul>
       
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item active">
+      <div class="genre-container">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 genre-list">
+      <li class="nav-item active genre-item">
       <a class="nav-link" href="#action">action</a>
     </li>
-    <li class="nav-item active">
-<!--      <a class="nav-link" href="#adventure">adventure</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#animation">animation</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#comedy">comedy</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#crime">crime</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#documentary">documentary</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#drama">drama</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#family">family</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#fantasy">fantasy</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#history">history</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#horror">horror</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#music">music</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#mystery">mystery</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#romance">romance</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#science-fiction">science-fiction</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#tv-movie">tv-movie</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#thriller">thriller</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#war">war</a>-->
-<!--    </li>-->
-<!--    <li class="nav-item active">-->
-<!--      <a class="nav-link" href="#western">western</a>-->
-<!--    </li>-->
-<!--     -->
-<!--      </ul>-->
-      
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#adventure">adventure</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#animation">animation</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#comedy">comedy</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#crime">crime</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#documentary">documentary</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#drama">drama</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#family">family</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#fantasy">fantasy</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#history">history</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#horror">horror</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#music">music</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#mystery">mystery</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#romance">romance</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#science-fiction">science-fiction</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#tv-movie">tv-movie</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#thriller">thriller</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#war">war</a>
+    </li>
+    <li class="nav-item active genre-item">
+      <a class="nav-link" href="#western">western</a>
+    </li>
+     
+      </ul>
+      </div>
       
       <form class="d-flex" role="search" id="search-form" autocomplete="off">
         <input class="form-control me-2" type="search" id="search-input" placeholder="Search" aria-label="Search">
